ce68982b73180e20d320c7d9274d031443a8fd3f
martin espinosa <221254@ids.upchiapas.edu.mx>
2024-09-09 23:53:39 -0600

    New modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/home_page.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+import 'package:url_launcher/url_launcher.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
+class Home extends StatelessWidget {
+  Home({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("View"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            final Uri phoneUri = Uri(scheme: 'tel', path: '21213123123');
+            if (await canLaunchUrl(phoneUri)) {
+              await launchUrl(phoneUri);
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('No se puede abrir la aplicación de teléfono')),
+              );
+            }
+          },
+          child: Text("Call me"),
+        ),
+      ),
+    );
+  }
+}
 
-
+void main() {
+  runApp(MyApp());
+}
